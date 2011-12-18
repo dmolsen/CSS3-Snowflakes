f41a3a25d68c931f7d1090f138fbdcf20d8db422
Dave Olsen <dmolsen@gmail.com>
2011-12-17 21:59:49 -0500

    Initial commit of CSS3 snowflake files

---
+++ README.md
@@ -0,0 +1,33 @@
+CSS3 Snowflakes
+===============
+
+About
+-----
+
+This is a simple demo of CSS3-based snowflakes. It is based on [the work of Estelle Weyl](http://www.standardista.com/sxsw/) ([@estellevw](http://twitter.com/estellevw/)). These snowflakes are dynamically created on page load and several effects (_e.g. opacity, size, speed_) are randomly added to each snowflake. The [code is available on GitHub](https://github.com/dmolsen/CSS3-Snowflakes).
+
+Demo
+----
+
+A [very simple demo of CSS3 snowflakes](http://dmolsen.com/css3-snowflakes/) is currently available.
+
+Browser Support
+---------------
+
+This demo should support Safari, Chrome, and Firefox.
+
+Requirements
+------------
+
+If you decide you want to initialize the snowflakes in your ```<head>``` tag then you'll need to use a simple library to tell you when the DOM is ready like [domReady](https://github.com/ded/domready). [domReady](https://github.com/ded/domready) is included and used in the distribution.
+
+To Use
+------
+
+If you would like to use CSS3 Snowflakes on your own site (_and who wouldn't?_) simply include ```snowflakes.js``` and ```snowflakes.css``` in your project. Modify ```snowflakes.js``` to include the proper references to the ```<div>``` that marks the overall container for your content as well as the ```<div>``` from which your snowflakes will fall. Then simply add:
+
+```var snowflakes = new Snowflakes();
+snowflakes.create(someNumber); 
+```
+
+to your page to generate the snowflakes.
---
+++ js/snowflakes.js
@@ -0,0 +1,86 @@
+/* initializes the various classes available for use when creating the snowflakes. classes are in snowflakes.css */
+function Snowflakes() {
+	this.snowID			= 1;
+	this.sizes			= new Array('', 'snowflakeSizeSM', 'snowflakeSizeMED', 'snowflakeSizeLRG');
+	this.speeds			= new Array('', 'snowflakeSpeedSlow', 'snowflakeSpeedMed', 'snowflakeSpeedFast');
+	this.opacities 		= new Array('', 'snowflakeOpacityFaint', 'snowflakeOpacityLight', 'snowflakeOpacityDark');
+	this.delays			= new Array('', 'snowflakeDelay1', 'snowflakeDelay2', 'snowflakeDelay3', 'snowflakeDelay4', 'snowflakeDelay5', 'snowflakeDelay6');
+	
+	/* these need to be updated to reflect your outermost div (pageContainer) as well as your snowflakes container */
+	this.pageContainer	= document.getElementById('container');
+	this.snowflakesContainer = document.getElementById('snowflakesContainer');
+}
+
+/* simple random number generator */
+Snowflakes.prototype.randomFromTo = function(from,to) {
+	return Math.floor(Math.random() * (to - from + 1) + from);
+};
+
+/* finds the keyframe we want to update */
+/* from: http://blog.joelambert.co.uk/2011/09/07/accessing-modifying-css3-animations-with-javascript/*/
+Snowflakes.prototype.findKeyframeAnimation = function(a) {
+	var ss = document.styleSheets;
+    for (var i = ss.length - 1; i >= 0; i--) {
+        try {
+            var s = ss[i],
+                rs = s.cssRules ? s.cssRules : 
+                     s.rules ? s.rules : 
+                     [];
+
+            for (var j = rs.length - 1; j >= 0; j--) {
+                if ((rs[j].type === window.CSSRule.WEBKIT_KEYFRAMES_RULE || rs[j].type === window.CSSRule.MOZ_KEYFRAMES_RULE) && rs[j].name == a){
+                    return rs[j];
+                }
+            }
+        }
+        catch(e) { /* Trying to interrogate a stylesheet from another domain will throw a security error */ }
+    }
+    return null;
+};
+
+/* updates the Keyframe Height for the 'falling' animation in snowflakes.css so snowflakes fall the full height of a page */
+Snowflakes.prototype.updateKeyframeHeight = function() {
+	if (keyframes = this.findKeyframeAnimation("falling")) {
+		var height		= this.pageContainer.offsetHeight;
+		if ((window.innerHeight) > height) {
+			height 		= window.innerHeight;
+		}
+		if (keyframes.cssText.match(new RegExp('webkit'))) {
+			var newRule = "100% { -webkit-transform: translate3d(0,"+height+"px,0) rotate(360deg); }";
+		} else if (keyframes.cssText.match(new RegExp('moz'))) {
+			var newRule = "-moz-transform: translate(0,"+height+"px) rotate(360deg);";
+		}
+		keyframes.insertRule(newRule);
+	}
+}
+
+/* Snowflakes.create(N) - creates N number of snowflakes & randomly assigns classes from the sizes, speeds, opacities, & delays arrays */
+/* Snowflakes.moreSnow(N) - just an alias and just helpful as a simple way of referring to the function */
+Snowflakes.prototype.create = Snowflakes.prototype.moreSnow = function(snowflakeCount) {
+	var i 				= 0;
+	this.updateKeyframeHeight();
+	while (i < snowflakeCount) {
+		var snowflake	= document.createElement('i');
+		var size 		= this.sizes[this.randomFromTo(0, this.sizes.length-1)];
+		var speed		= this.speeds[this.randomFromTo(0, this.speeds.length-1)];
+		var opacity 	= this.opacities[this.randomFromTo(0, this.opacities.length-1)];
+		var delay		= this.delays[this.randomFromTo(0, this.delays.length-1)];
+		snowflake.setAttribute('id', 'snowId'+this.snowID);
+		snowflake.setAttribute('class', 'snowflake '+size+' '+speed+' '+opacity+' '+delay);
+		snowflake.setAttribute('style','left: '+this.randomFromTo(0, 100)+'%;');
+		snowflakesContainer.appendChild(snowflake);
+		i++;
+		this.snowID++;
+	}
+};
+
+/* Snowflakes.lessSnow(N) - removes N number of snowflakes from the DOM */
+Snowflakes.prototype.lessSnow = function(snowflakeCount) {
+	if (this.snowflakesContainer.childNodes.length > snowflakeCount) {
+		var snowRemoveCount = 0;
+		while (snowRemoveCount < snowflakeCount) {
+			this.snowflakesContainer.removeChild(this.snowflakesContainer.lastChild);
+			snowRemoveCount++;
+		}
+	}
+}
